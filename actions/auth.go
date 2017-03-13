085b38d51cb62a05137e063b1c11cf70a425a4cf
lyckade <andrea@apxp.info>
2017-03-13 18:06:15 +0100

    Editing the AuthCallback

--- actions/auth.go
+++ actions/auth.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/as27/setenv"
@@ -14,7 +15,6 @@ import (
 func init() {
 	gothic.Store = App().SessionStore
 	setenv.File(".env")
-
 	goth.UseProviders(
 		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), fmt.Sprintf("%s%s", App().Host, "/auth/github/callback")),
 	)
@@ -26,5 +26,16 @@ func AuthCallback(c buffalo.Context) error {
 		return c.Error(401, err)
 	}
 	// Do something with the user, maybe register them/sign them in
-	return c.Render(200, r.JSON(user))
+	// Adding the userID to the session to remember the logged in user
+	session := c.Session()
+	session.Set("userID", user.UserID)
+	err = session.Save()
+	if err != nil {
+		return c.Error(401, err)
+	}
+	// The default value jus renders the data we get by GitHub
+	// return c.Render(200, r.JSON(user))
+
+	// After the user is logged in we add a redirect
+	return c.Redirect(http.StatusMovedPermanently, "/secure")
 }
